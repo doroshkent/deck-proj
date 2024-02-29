ca083656c9d35b09b6feaeb2929b412b1e40d40c
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-29 13:58:29 +0100

    Update tsconfig and add deck API
    The file `decks-api.ts` under the `src/features/decks` directory has been updated to include a new API for fetching decks. The API was then utilized in the `DecksList.tsx` file for fetching decks data on component mount.

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,11 @@
 import s from './DecksList.module.css'
+import { useEffect } from 'react'
+import { decksAPI } from '../decks-api.ts'
 
 export const DecksList = () => {
+  useEffect( () => {
+    decksAPI.getDecks()
+      .then(() => console.log("success"))
+  }, [] )
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -1,8 +1,43 @@
 import axios from 'axios'
 
-export const instance = axios.create({
+export const instance = axios.create( {
   baseURL: 'https://api.flashcards.andrii.es',
   headers: {
     'x-auth-skip': true,
   },
-})
+} )
+
+export const decksAPI = {
+  getDecks() {
+    return instance.get<GetDecksResponseType>( '/v2/decks' )
+  },
+}
+
+// types
+// entity types
+type DeckType = {
+  author: {
+    id: string
+    name: string
+  }
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  cover: string
+  created: Date
+  updated: Date
+  cardsCount: number
+}
+type PaginationType = {
+  currentPage: number
+  itemsPerPage: number
+  totalPages: number
+  totalItems: number
+}
+
+// response types
+type GetDecksResponseType = {
+  items: DeckType[]
+  pagination: PaginationType
+}
