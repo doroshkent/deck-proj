79ab8e14fa5d762d4e758a71898dea3c6d142972
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-03-08 14:50:01 +0100

    Add generic utility components and helper function

---
+++ src/tasks/task-2.tsx
@@ -0,0 +1,27 @@
+type MyComponentProps<T> = {
+  items: T[]
+  defaultItem: T
+}
+function MyComponent<T>(props: MyComponentProps<T>) {
+  console.log(props)
+  return <p>some content</p>
+}
+
+const App = () => {
+  const users: User[] = [
+    { name: 'Bilbo', age: 111 },
+    { name: 'Frodo', age: 33 },
+  ]
+
+  return (
+    <>
+      <MyComponent items={['react', 'typescript']} defaultItem={'JUST STRING'} />
+      <MyComponent items={users} defaultItem={users[0]} />
+    </>
+  )
+}
+
+type User = {
+  name: string
+  age: number
+}
